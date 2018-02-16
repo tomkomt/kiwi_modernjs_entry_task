32ba82cdb38f1f5247e257a5bae2a137e19f8a0b
Marek Tomko <MTomko@sk.ibm.com>
2018-02-16 09:58:22 +0100

    feat(consts): Add url to airline logos

--- client/source/consts/consts.js
+++ client/source/consts/consts.js
@@ -1,5 +1,6 @@
 module.exports = {
     apiUrl: 'https://api.skypicker.com',
+    imgUrl: 'https://images.kiwi.com/airlines/32',
     dateFormats: {
         flightsRequestFormat: 'DD/MM/YYYY',
         searchFormFormat: 'D.M. YYYY'
